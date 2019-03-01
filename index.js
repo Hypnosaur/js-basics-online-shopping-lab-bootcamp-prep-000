e38ba95a626f6fae8c00eb77d144167c942c68cd
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 20:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,6 +47,10 @@ function viewCart() {
 
 function total() {
   // write your code here
+  let total = 0
+  for (let i = 0; i < cart.length; i++){
+    total = total + 
+  }
 }
 
 function removeFromCart(item) {
