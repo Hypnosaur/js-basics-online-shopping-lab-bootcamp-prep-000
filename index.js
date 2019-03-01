50b3ff261beac5a5390fc3c79a1ae270c6e752fd
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 20:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,6 +56,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for( let i = 0; i < cart.length; i++){
+    if(cart[i].itemName === item ){
+      cart[i].splice()
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
