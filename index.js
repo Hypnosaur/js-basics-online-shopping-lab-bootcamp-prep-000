9888d5c9493e3e6e47ecce2c2cce22299c6ade12
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 20:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === ""){
-    console.log("Sorry, we don't have a credit card on file for you..")
-
+    return "Sorry, we don't have a credit card on file for you..."
   }
 }
