c2e682f09a3485fa78ea809cc28e1c5d1090242c
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 20:58:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined ){
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    cost = total()
     cart.length = 0
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   }
 }
